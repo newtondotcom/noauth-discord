440c0dc8b171ab73648cc07ed2beddf19db91e9b
newtondotcom <asphalt9fr@gmail.com>
2023-08-10 23:26:07 +0200

    receive user from django

--- constants.js
+++ constants.js
@@ -3,13 +3,14 @@ const Constants = {
   prefix: ";",
   owners: ["423151303057735681"],
   authLink: "https://discord.com/api/oauth2/authorize?client_id=1132681850528202782&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fcallback&response_type=code&scope=guilds%20guilds.join%20identify%20email",
-  port : 8000,
+  port : 5000,
   clientId: '1132681850528202782',
   clientSecret: 'n4NZENs8p76o9J3FPLkK5woBFYQ0cgf3',
   redirectUri: 'http://localhost:8000/callback',
   scope: 'identify guilds.join guilds email',
   guildId: '978694633213280256',
   color : 1752220,
+  webhook : 'https://discord.com/api/webhooks/1138932963464192070/naN-uxPR1AEU9fBX_tJTv7RIJOzBl9YLpsfohH-otcceLHlSSI7ttxi2I7ndXgGZuUg-',
 };
 
 module.exports = Constants;
--- index.js
+++ index.js
@@ -13,12 +13,53 @@ const bodyParser = require('body-parser');
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const axios = require('axios');
 
-app.use(bodyParser.text());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /* EXPRESS JS */
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/register_user', function (req, res) {
+  const access_token = req.body.access_token;
+  const refresh_token = req.body.refresh_token;
+  const username = req.body.username;
+  const mail = req.body.mail;
+  const userID = req.body.userID;
+
+  const objectData = fs.readFileSync('./object.json');
+  const objectJson = JSON.parse(objectData);
+
+  const newUser = {
+    userID: userID,
+    avatarURL : '',
+    username: username,
+    access_token: access_token,
+    refresh_token: refresh_token,
+    mail : mail,
+  };
+
+  objectJson.push(newUser);
+  fs.writeFileSync('./object.json', JSON.stringify(objectJson));
+
+  console.log(`[+] ${username} - ${userID}`);
+  fetch(Constants.webhook, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      avatar_url: '',
+      embeds: [
+        {
+          color: Constants.color,
+          title: `${newUser.username} - ${newUser.userID}`,
+          description: `\`\`\`diff\n- New User\n\n- Pseudo: ${newUser.username}\n\n- ID: ${newUser.userID}\`\`\``,
+        },
+      ],
+    }),
+  });
+  return res.json({ status: 'ok' });
 });
 
 app.get('/allAuth-noauth', async (req, res) => {
@@ -159,9 +200,15 @@ client.on("ready", () => {
   });
 });
 
-client.on('guildMemberAdd', (member) => {
+client.on('guildMemberAdd', async (member) => {
   const userId = member.user.id;
-  console.log(`New member joined! User ID: ${userId}`);
+  const data = await fetch(`http://127.0.0.1:8000/join/?userID=${userId}&guildID=${Constants.guildId}`, { method: 'POST' });
+  if (data.status !== 200) {
+    console.log(`Error while adding user ${userId} to the database. Status: ${data.status}`);
+    return;
+  } else {
+    console.log(`New member joined! User ID: ${userId}`);
+  }
 });
 
 client.on('guildMemberRemove', (member) => {
