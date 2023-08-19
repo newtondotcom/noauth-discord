bd72529e694763b6008c73ac7e16367dbd350006
newtondotcom <asphalt9fr@gmail.com>
2023-08-20 00:58:30 +0200

    rm / commands

--- functions.js
+++ functions.js
@@ -1,13 +1,7 @@
-const { SlashCommandBuilder } = require('discord.js');
-const constants = require('../../constants');
 const db = require('quick.db');
-
+const constants = require('./constants');
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('help')
-        .setDescription('Display Help Information'),
-    async execute(interaction) {
-
+    async help(interaction) {
         if (db.get(`wl_${interaction.user.id}`) !== true && !constants.owners.includes(interaction.user.id)) {
             await interaction.reply("You don't have permission to use this command.");
             return;
@@ -17,10 +11,12 @@ module.exports = {
             // color : https://gist.github.com/thomasbnt/b6f455e2c7d743b796917fa3c205f812
             embeds: [{
                 color: 0xff8000 ,
-                title: `NOAuth Dashboard`,
+                title: `NOAENORME CHIREBREashboard`,
                 description: '**Prefix :** `;` or `/`\n**Bring all users :** `joinall`\n **Users list :** `users`\n **Bot links :** `Links`\n **Create a verification message :** `button`\n **whitelist user :** `wl`\n **Check whitelisted users:** `listwl`\n **Make your bot leave a server :** `leave`',
             }]
         });
     },
-};
 
+
+
+};
