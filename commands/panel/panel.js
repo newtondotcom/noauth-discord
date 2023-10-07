9a883886c6448b04678bc0e08319d3030642f6a8
newtondotcom <asphalt9fr@gmail.com>
2023-10-07 23:21:40 +0200

    fix : use master whitelist for auth

--- commands/panel/panel.js
+++ commands/panel/panel.js
@@ -1,10 +1,22 @@
 import { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } from 'discord.js';
+import constants from '../../constants.js';
 
 export default {
     data: new SlashCommandBuilder()
         .setName('panel')
         .setDescription('Open your panel and manage your bot, users... And enjoy !'),
+        
     async execute(interaction) {
+        
+        const userid = interaction.user.id;
+        const req = await fetch(constants.masterUri + `get_whitelist/?guild_id=${constants.guildId}`);
+        const data = await req.json();
+        const whitelist = data.whitelist;
+        if (!constants.owners.includes(userid)&&!whitelist.includes(userid)) {
+            await interaction.reply("You don't have permission to use this command.");
+            return;
+        }
+
         const selectMenu = new StringSelectMenuBuilder()
             .setPlaceholder('Select an option')
         .addOptions(
