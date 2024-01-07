25d7b0336b7163aa647c0588cbec0ec2275ba3f7
newtondotcom <asphalt9fr@gmail.com>
2024-01-07 11:18:43 +0100

    fix and test for access token

--- test.js
+++ test.js
@@ -1,38 +1,17 @@
-import constants from './constants.js';
-
-export async function sendWebhook(title,body){
-    const webhook = constants.webhook;
-    const data = {
-        "embeds": [
-            {
-                "title": title,
-                "description": body,
-                "color": 16711680
-            }
-        ]
-    };
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    };
-    await fetch(webhook, options);
-}
-
-export async function testUsers(client) {
-    client.guilds.cache.forEach(async guild => {
-      if (guild.id !== constants.guildId) {console.log(guild.id); return};
-      const response = await fetch(`${constants.masterUri}get_members?guild_id=${constants.guildId}`);
-      const membersData = await response.json();
-      // Loop through users in objectJson and call testToken for each user
-      for (const user of membersData.members) {
-        await testToken(guild.id,user.userID, user.access_token, user.refresh_token);
-      }
-      sendWebhook("Testing users ... ", "Bot linked to \`"+constants.guildId + "\` has tested \`" +membersData.members.length+"\` users for \`"+guild.id +"\`");
-    });
-  }
+var constants = {
+    token: "MTEzMjY4MTg1MDUyODIwMjc4Mg.GaVZmq.oW55RQNWESddqaIgBhBm3FD-M7tFWmGMaURZz8",
+    owners: ["982703621479206952","423151303057735681","982703621479206952"],
+    authLink: "https://discord.com/api/oauth2/authorize?client_id=1132681850528202782&redirect_uri=https%3A%2F%2Fnewton-creations.site%2Fverif%2Ftest%2F&response_type=code&scope=guilds%20guilds.join%20identify%20email",
+    port: 5000,
+    clientId: '1132681850528202782',
+    clientSecret: 'xAh_SlMtBIsDCVdHknL0cWh1hv2Mmkjh',
+    scope: 'identify guilds.join guilds email',
+    guildId: '1092562334498705530',
+    color: 15844367,
+    webhook: 'https://discord.com/api/webhooks/1138932963464192070/naN-uxPR1AEU9fBX_tJTv7RIJOzBl9YLpsfohH-otcceLHlSSI7ttxi2I7ndXgGZuUg-',
+    name: 'test',
+    masterUri: 'http://localhost:8000/'
+};
   
   export async function testToken(guild_id, user_id, access_token, refresh_token) {
     const response = await fetch("https://discord.com/api/users/@me", {
@@ -45,16 +24,16 @@ export async function testUsers(client) {
     });
     const data = await response.json();
     if (response.ok) {
-      console.log("user still valid for "+user_id);
+      console.log("C EST BON");
       return;
     } else {
       try {
         const [newAccessToken, newRefreshToken ] = await renewToken(constants.clientId, constants.clientSecret, refresh_token);
         const response = await fetch(`${constants.masterUri}update_access_token/?user_id=${user_id}&access_token=${newAccessToken}&refresh_token=${newRefreshToken}&guild_id=${guild_id}`);
         const datas = await response.text();
-        console.log("Token updated for : "+user_id);
+        console.log("token updated for"+user_id);
       } catch (error) {
-        console.log(user_id+" is invalid, should be deleted");
+        console.log("error in test Token function export to delete");
         console.log(error);
         //const response = await fetch(`${constants.masterUri}dl_user/?user_id=${user_id}&guild_id=${guild_id}`);
         //const datas = await response.json(); // Use await to get the JSON response
@@ -86,5 +65,24 @@ export async function testUsers(client) {
     } 
   
     const data = await response.json();
+    console.log(data);
+    console.log(data.access_token, data.refresh_token)
     return [data.access_token, data.refresh_token];
   }
+
+let guildid = "1193140021520252988"
+let access_token = ""
+let refresh_token = ""
+let user_id = "1045784030957817886"
+testToken(guildid, user_id, access_token, refresh_token)
+
+const [newAccessToken, newRefreshToken ] = await renewToken(constants.clientId, constants.clientSecret, refresh_token);
+const response = await fetch(`${constants.masterUri}update_access_token/?user_id=${user_id}&access_token=${newAccessToken}&refresh_token=${newRefreshToken}&guild_id=${guildid}`);
+const datas = await response.text();
+console.log("token updated for"+user_id);
+console.log(newAccessToken, newRefreshToken)
+console.log("token updated for"+user_id);
+
+/*
+
+*/
