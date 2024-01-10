13223cc6c91034595ac0a7c2739cd97ebc046466
newtondotcom <asphalt9fr@gmail.com>
2024-01-10 22:08:01 +0100

    custom content for whitelist

--- functions/functions_wl.js
+++ functions/functions_wl.js
@@ -14,10 +14,17 @@ export default {
           content += `\`${whitelist.indexOf(whitelist[i]) + 1}.\` ${interaction.client.users.cache.get(whitelist[i].user_id).tag} (\`${interaction.client.users.cache.get(whitelist[i].user_id).id}\`), added by \`${whitelist[i].added_by}\` at \`${whitelist[i].date}\` \n`;
         }
 
+        if (content == "") {
+          content = "No whitelisted users for now, try to add one";
+        } else {
+          content = `**${content}**`;
+        
+        }
+
         interaction.update({
           embeds: [{
             title: "Whitelisted Users",
-            description: `**${content}**`,
+            description: content,
             color: constants.color,
           }]
         });
