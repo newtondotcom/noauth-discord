13afbb367165edc385793b59a1fc69378632373f
RaichuWK <rahaliryan75@gmail.com>
2024-03-12 16:23:43 +0100

    ajout des regles pour chaque user wl (menu)

--- functions/functions_wl.js
+++ functions/functions_wl.js
@@ -1,3 +1,4 @@
+import {ModalBuilder,TextInputBuilder, TextInputStyle , ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } from 'discord.js';
 import constants from '../constants.js';
 import fetch from 'node-fetch';
 
@@ -31,5 +32,128 @@ export default {
         });
 
     },
+
+    //// REGLES
+
+    async wlrulesmanage(interaction) {
+      const userid = interaction.user.id;
+      if (!constants.owners.includes(userid)) {
+          await interaction.update("You don't have permission to use this command.");
+          return;
+      }
+      const selectMenu = new StringSelectMenuBuilder()
+          .setPlaceholder('Select an option')
+          .addOptions(
+              new StringSelectMenuOptionBuilder()
+                  .setEmoji('➕')
+                  .setLabel('Add Rule')
+                  .setDescription('Give access to your bot')
+                  .setValue('wlrules'),
+          )
+          .addOptions(
+            new StringSelectMenuOptionBuilder()
+                .setEmoji('📕')
+                .setLabel('Remove rule')
+                .setDescription('Give access to your bot')
+                .setValue('wlrulesrmv'),
+          )
+          .addOptions(
+            new StringSelectMenuOptionBuilder()
+                .setEmoji('📕')
+                .setLabel('Rules list')
+                .setDescription('Give access to your bot')
+                .setValue('wlruleslist'),
+          )
+          .addOptions(
+            new StringSelectMenuOptionBuilder()
+                .setEmoji('⏪')
+                .setLabel('Go back')
+                .setValue('managewl'),
+        )
+          .setCustomId('selectBot');
+
+      const row = new ActionRowBuilder().addComponents(selectMenu);
+      await interaction.update({ content: '', components: [row] });
+  },
+
+
+    //// REMOVE RULES
+    async wlrulesrmv(interaction) {
+      const userid = interaction.user.id;
+      if (!constants.owners.includes(userid)) {
+          await interaction.update("You don't have permission to use this command.");
+          return;
+      }
+      const selectMenu = new StringSelectMenuBuilder()
+          .setPlaceholder('Select an option')
+          .addOptions(
+            new StringSelectMenuOptionBuilder()
+                .setEmoji('🔴')
+                .setLabel('Rule 1')
+                .setDescription('Give access to your bot')
+                .setValue('wlrulesmanage'),
+          )
+          .setCustomId('selectBot');
+
+      const row = new ActionRowBuilder().addComponents(selectMenu);
+      await interaction.update({ content: '', components: [row] });
+  },
+
+
+
+
+
+
+    ///FOMULAIRE
+    async wlrules(interaction) {
+      const query = await fetch(constants.masterUri+'get_button/?guild_id='+constants.guildId, {method: 'GET',headers: constants.header})
+      const datat = await query.json()
+      const data = datat.button[0]
+
+      const modal = new ModalBuilder()
+          .setCustomId('wlrules')
+          .setTitle('Wl Restrictions');
+
+
+      const footer = new TextInputBuilder()
+          .setCustomId('footer')
+          .setValue(data.footer)
+          .setLabel("Rule name")
+          .setRequired(true)
+          .setStyle(TextInputStyle.Short);
+
+      const content = new TextInputBuilder()
+          .setCustomId('content')
+          .setValue(data.content)
+          .setLabel("User ID")
+          .setRequired(true)
+          .setStyle(TextInputStyle.Short);
+
+          const name = new TextInputBuilder()
+          .setCustomId('name')
+          .setValue(data.name)
+          .setLabel("Session Max /day ?")
+          .setRequired(false)
+          .setStyle(TextInputStyle.Short);
+
+      const title = new TextInputBuilder()
+          .setCustomId('title')
+          .setValue(data.title)
+          .setLabel("Maximum number of users allowed to join ?")
+          .setRequired(false)
+          .setStyle(TextInputStyle.Short);
+
+          const footerActionRow = new ActionRowBuilder().addComponents(footer);
+          const nameActionRow = new ActionRowBuilder().addComponents(name);
+          const titleActionRow = new ActionRowBuilder().addComponents(title);
+          const contentActionRow = new ActionRowBuilder().addComponents(content);
+  
+          modal.addComponents(footerActionRow);
+          modal.addComponents(contentActionRow);
+          modal.addComponents(nameActionRow);
+          modal.addComponents(titleActionRow);
+
+      await interaction.showModal(modal);
+  }
 
 };
