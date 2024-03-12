b4f99fe99be919cd68342ddade5524c3f2b262f9
RaichuWK <rahaliryan75@gmail.com>
2024-03-12 18:15:20 +0100

    style message verification

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/register_user/', async (req, res) => {
     const member = await guild.members.fetch(id);
     member.roles.add(role);
     const guildName = guild.name;
-    body = `-👤 Username: ${member.user.username}#${member.user.discriminator}\n - 🆔 User ID: ${member.id}\n  - 🏚️ Server: ${guildName}\n - Current count: ${count}`
+    body = `-👤 Username: **${member.user.username}#${member.user.discriminator}**\n - 🆔 User ID: **${member.id}**\n  - 🏚️ Server: **${guildName}**\n - Current count: **${count}**`
   } catch (error) {
     body = `Current count: ${count}`
     console.log("Server "+ server +" not found");
