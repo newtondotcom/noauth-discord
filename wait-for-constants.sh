4ab208645655695fab239ed02e7940fb755433e5
newtondotcom <asphalt9fr@gmail.com>
2024-03-19 19:52:03 +0100

    exec js file using bun

--- wait-for-constants.sh
+++ wait-for-constants.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 sleep 2
 
-node scripts/generateConstants.js
+bun scripts/generateConstants.js
 
 # Wait for the generateConstants.js script to finish (e.g., until a specific file exists)
 while [ ! -f constants.js ]; do
   sleep 1
 done
 
-node deploy-commands.js
+bun deploy-commands.js
 sleep 2
 
 # Start the main application (index.js)
